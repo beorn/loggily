a2ff5d7f1746f435628f21ca268f5010917c77fc
Bjørn Stabell <bjorn@stabell.org>
2026-03-31 16:17:07 -0700

    docs: Bjorn → Bjørn + remove km references from public docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -121,7 +121,7 @@ export default defineConfig({
 
     footer: {
       message: 'Used by <a href="https://silvery.dev">Silvery</a>, <a href="https://termless.dev">Termless</a>, and <a href="https://terminfo.dev">terminfo.dev</a>',
-      copyright: 'Built by <a href="https://beorn.codes">Bjorn Stabell</a>'
+      copyright: 'Built by <a href="https://beorn.codes">Bjørn Stabell</a>'
     },
   },
 })
